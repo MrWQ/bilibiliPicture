851bd128607ec13ff05e4b74b9d272acbd0adf41
MrWQ <31604279+MrWQ@users.noreply.github.com>
2018-05-26 17:16:40 +0800

    版本2.0

--- b2.py
+++ b2.py
@@ -11,30 +11,37 @@ else:
 
 def download(number):
     base_url="https://www.bilibili.com/video/av"
-    base_http="http:"
+    # base_http="http:"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36 LBBROWSER"}
     url=base_url+str(number)
     # print(url)
     text=requests.get(url,headers=headers).text
     # print(text)
-    pattern=re.compile(r'<img src="(.*?)" style="display:none;" class="cover_image"/>',re.S)
+    # 重新定义正则表达式来寻找封面直链
+    pattern=re.compile(r'itemprop="image" content="(.*?)"/>',re.S)
     picture=re.findall(pattern,text)
-    if picture:#封面存在
+    # print(picture)
+    if picture :#视频地址存在
         for i in picture:
-            http=base_http+str(i)
-            #print(http)#图片地址
-            print('正在下载:av'+ number +'正在自动保存在当前页bilibili目录下')
+            if(i):#封面存在
+                # http=base_http+str(i)
+                http=i
+                # print(http)#图片地址
+                print('正在下载:av'+ number +'正在自动保存在当前页bilibili目录下')
 
-            try:
-                pic = requests.get(http, timeout=10)
-            except requests.exceptions.ConnectionError:
-                print('【错误】当前图片无法下载')
-                continue
+                try:
+                    pic = requests.get(http, timeout=10)
+                except requests.exceptions.ConnectionError:
+                    print('【连接超时】当前图片无法下载')
+                    continue
 
-            f=open(str(number)+'.jpg','wb')
-            f.write(pic.content)
-            f.close()
+                f=open(str(number)+'.jpg','wb')
+                f.write(pic.content)
+                f.close()
+            else:
+                print("视频不存在！")
+
     else:
         print("视频不存在！")
 
